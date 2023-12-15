64e1d6d2a8d4914eb4c5753624eb2dfa265aaa67
Eray <erayguler6757@gmail.com>
2023-12-15 17:17:32 +0300

    Add description on the new columns

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ with col1:
 with col2:
     st.title("Eray Güler")
     content = """
-    As a computer engineer with a Udemy certification in Java programming, I have gained valuable experience in software
-     development and object-oriented programming (OOP) skills. I am currently pursuing my bachelor's and master's 
-     degrees in computer engineering at Manisa Celal Bayar University, where I am learning the latest technologies 
-     and methods in the field. My goal is to apply my knowledge and skills to create innovative and impactful software
-      solutions that can enhance the lives of people and the performance of businesses. I am passionate about learning 
-      new things, solving problems, and working with diverse and collaborative teams.
+    As a computer engineer, I have gained valuable experience in software development and object-oriented programming 
+    (OOP) skills. I am currently pursuing my bachelor's and master's degrees in computer engineering at Manisa Celal 
+    Bayar University. My goal is to apply my knowledge and skills to create innovative and impactful software solutions 
+    that can enhance the lives of people and the performance of  businesses. I am passionate about learning new things,
+    solving problems, and working with diverse and collaborative teams.
     """
     st.info(content)
+
+content2 = """Below you can find some of the apps I have built in Python. Feel free to contact me!"""
+
+st.write(content2)
